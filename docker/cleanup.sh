6525a35590c48c3f1fc67bcf120c75dcf79a6b03
homosepian <amitnira@gmail.com>
2016-03-22 19:23:48 -0300

    Intermediate - deploy and cleanup scripts

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -2,6 +2,8 @@
 
 docker stop escluster && docker rm escluster
 docker stop izagent && docker rm izagent
-docker network rm stream_net
+docker stop lands_rest && docker rm lands_rest
+docker stop lands_ser && docker rm lands_ser
+docker network rm informiz_net
 
 
--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,12 +3,18 @@
 # build images
 docker build -t flume -f $(pwd)/flume/Dockerfile $(pwd)/flume
 docker build -t elasticsearch -f $(pwd)/elasticsearch/Dockerfile $(pwd)/elasticsearch
+docker build -t lands_ser -f $(pwd)/lands-ser/Dockerfile $(pwd)/lands-ser
+docker build -t lands_rest -f $(pwd)/lands-rest/Dockerfile $(pwd)/lands-rest
 
-# create a bridge network for streaming events into elasticsearch
-docker network create stream_net
-# for multiple hosts: docker network create -d overlay stream_net
+# create a bridge network for the app
+docker network create informiz_net
+# for multiple hosts: docker network create -d overlay informiz_net
 
 # start elasticsearch
-docker run -d --net=stream_net --name=escluster -p 9300:9300 -p 9200:9200 elasticsearch
+docker run -d --memory-swappiness=0 --net=informiz_net --name=escluster -p 9300:9300 -p 9200:9200 elasticsearch
 #start flume
-docker run -d --net=stream_net --name=izagent -p 44444:44444 -t flume
+docker run -d --net=informiz_net --name=izagent -p 44444:44444 -t flume
+#start landscape service on host, where neo4j is running. TODO: run neo4j in container
+docker run -d --net=host --name=lands_ser lands_ser
+#start landscape endpoint
+docker run -d --net=host --name=lands_rest -p 8080:8080 lands_rest
